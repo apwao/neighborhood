9afaa20b7a1ee04fa186b7df1a1911649341795d
Sowayi <sapwao@gmail.com>
2019-07-07 06:02:00 -0400

    Add a Neighborhood model to facilitate saving of different neighborhoods in the database

--- estate/models.py
+++ estate/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighborhood(models.Model):
+    """
+    class Neighborhood to facilitate creation of instances of a neighborhood
+    """
+    name=models.CharField(max_length=200)
+    location=models.CharField(max_length=200)
+    population=models.PositiveIntegerField()
+
+    def create_neighborhood(self):
+        """
+        create_neighborhood method to help create a neighborhood
+        and commit it to the database
+        """
+        
