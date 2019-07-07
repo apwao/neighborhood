1bedecdc533d85a94ec41145a8cfab308390d83d
Sowayi <sapwao@gmail.com>
2019-07-07 14:48:22 -0400

    Add Post model to enable saving of post details to the database

---
+++ chat/forms.py
@@ -0,0 +1,12 @@
+from .models import Post
+from django import forms
+
+class PostForm(forms.ModelForm):
+    """
+    Class ProjectForm to enable a user to make an announcement in the neighborhood chatroom
+    """
+    class Meta:
+        model=Post
+        fields=('title','post')
+        
+        
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from estate.models import Neighborhood
+
 
 # Create your models here.
+class Post(models.Model):
+    """
+    class Post to enable a user create and submit a post to be viewed by other users in a neighborhood
+    """
+    title=models.CharField(max_length=200)
+    post=models.TextField()
+    upload_date=models.DateTimeField(auto_now_add=True)
+    posted_by=models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    neighborhood_id=models.ForeignKey(Neighborhood,on_delete=CASCADE)
+    
+    def save_post(self):
+        """
+        save post method to save post details provided 
+        by the user to the database
+        """
+        self.save()
+        
+    def delete_post(self):
+        """
+        delete_post method to remove a post from the database
+        """
+        self.delete()
+
+    
