cf799546d3aeb09475f54fc54c1aed15b50edf00
Sowayi <sapwao@gmail.com>
2019-07-07 06:17:49 -0400

    Add methods create and delete neighborhoods to the Neighborhood class

--- estate/models.py
+++ estate/models.py
@@ -15,4 +15,23 @@ class Neighborhood(models.Model):
         create_neighborhood method to help create a neighborhood
         and commit it to the database
         """
+        self.save()
         
+    def delete_neighborhood(self):
+        """
+        delete_neighborhood method to help remove an existing
+        neighborhood from the database
+        """
+        self.delete()
+        
+    @classmethod    
+    def find_neighborhood(cls,id):
+        """
+        find_neighborhood method to search through the neighborhood table in the
+        database and return the specified neighborhood
+        """
+        hood=cls.objects.filter(id=id)
+        return hood
+        
+    def __str__(self):
+        return self.name
