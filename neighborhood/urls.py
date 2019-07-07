3792923a04c5abb54d961a09e74742acf43e01d2
Sowayi <sapwao@gmail.com>
2019-07-07 05:38:36 -0400

    Register urls for the estate app

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
+    url(r'',include('estate.urls')),
 ]
