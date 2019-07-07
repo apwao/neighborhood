0a9723dd82d6a2426a5c3939d4ad09a3c9f1dd38
Sowayi <sapwao@gmail.com>
2019-07-07 05:38:05 -0400

    Add login redirect setting to render landing page template after signing in

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authenticate',
+    'estate',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +82,10 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighborhood',
+        'USER': 'sowayi',
+        'PASSWORD':'Sowasse',
     }
 }
 
@@ -119,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# LOGIN_REDIRECT_URL='//'
