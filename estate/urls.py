5ceb647b6dcf4d88f6beb8f86f329367d8d76705
Sowayi <sapwao@gmail.com>
2019-07-07 11:12:20 -0400

    Add url patterns for displaying the business form and business details

--- estate/urls.py
+++ estate/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns=[
-    url('^$', views.homepage, name='homepage'),
-    url('^view_businesses/(\d+)/', views.view_businesses,name='view_businesses')
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^view_businesses/(\d+)/', views.view_businesses,name='view_businesses'),
+    url(r'^process_bizform/$', views.process_biz_form, name='process_biz_form'),
+    url(r'^business_form/$', views.business_form, name='business_form')
 ]
