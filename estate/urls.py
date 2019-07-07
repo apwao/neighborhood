7b8d41db37a0dc1852dfe6d6c299096c90d279ad
Sowayi <sapwao@gmail.com>
2019-07-07 14:16:00 -0400

    Add url patterns to enable user to create a profile and view their profile

--- estate/urls.py
+++ estate/urls.py
@@ -7,5 +7,9 @@ urlpatterns=[
     url(r'^$', views.homepage, name='homepage'),
     url(r'^view_businesses/(\d+)/', views.view_businesses,name='view_businesses'),
     url(r'^process_bizform/$', views.process_biz_form, name='process_biz_form'),
-    url(r'^business_form/$', views.business_form, name='business_form')
+    url(r'^business_form/$', views.business_form, name='business_form'),
+    url(r'^profile_form/$',views.profile_form, name='profile_form'),
+    url(r'^view_profile/$',views.view_profile, name='view_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
