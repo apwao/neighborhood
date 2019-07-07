e9fdc06c045ca1911a4148629c5b46c226fb3761
Sowayi <sapwao@gmail.com>
2019-07-07 07:58:22 -0400

    Add view_businesses view function to display businesses within a given neighborhood

--- estate/views.py
+++ estate/views.py
@@ -7,5 +7,16 @@ def homepage(request):
     """
     homepage view function to display the landing page of the application
     """
+    neighborhoods=Neighborhood.objects.all()
     
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html',{'neighborhoods',neighborhoods})
+
+@login_required(login_url='/accounts/login/')
+def view_businesses(request,neighborhood_id):
+    """
+    view_businesses function that displays registered businesses within a neighborhood
+    """
+    
+    neighborhood_business=Business.objects.filter(neighborhood_id=neighborhood_id)
+    
+    return render(request, 'businesses.html',{'neighborhood_business':neighborhood_business})
